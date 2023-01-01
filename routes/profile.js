46df902cb96242b2e8e441f116a0a2170312206f
pillared <>
2023-01-01 02:37:01 -0600

    origiin

--- routes/profile.js
+++ routes/profile.js
@@ -12,7 +12,7 @@ require('dotenv').config()
 //   },
 // }
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: `${process.env.CLIENT_URL}:${process.env.CLIENT_URL_PORT}`,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 const {
